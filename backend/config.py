d5a69cfc285557ba68cc0283577e8b0d9d5df889
Robs179 <robin.carey17@gmail.com>
2025-03-01 12:26:27 +0000

    Add configuration for TFL API integration

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+class Config:
+    TFL_KEY = "76e11c6a4cdd48f6bf7dd93a99510482"
+    TFL_URL = "https://api.tfl.gov.uk"
+    
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from config import Config
 
 app = FastAPI()
 
+# Access the API key
+api_key = Config.API_KEY
+
 @app.get("/")
-def root():
-    return {"message": "Welcome to Travel Optimizer!"}
+def read_root():
+    return {"API_KEY": api_key}

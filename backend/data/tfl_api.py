c1a0943a69a283539649578ae5e08e3da8cc8644
Robs179 <robin.carey17@gmail.com>
2025-03-02 11:59:42 +0000

    Remove unecessary files

--- backend/data/tfl_api.py
+++
@@ -1,6 +0,0 @@
-def fetch_tfl_fares():
-    """
-    Fetch fares from TfL API.
-    """
-    # ...your API calling logic...
-    return {"fare": 2.50}
--- backend/data/ticket_splitter.py
+++
@@ -1,6 +0,0 @@
-def calculate_cheapest_tickets(fares):
-    """
-    Calculate the cheapest ticket combinations given a list of fares.
-    """
-    # ...logic to compute cheapest combination...
-    return min(fares, key=lambda x: x.get("fare", float("inf")))

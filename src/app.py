bfa1133746416bab946dae5311b22aaa2d5484a0
Robs179 <robin.carey17@gmail.com>
2025-03-02 04:41:03 +0000

    Prettified

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+from config import Config
+from utils.route_parser import RouteParser as rp
 
 app = Flask(__name__)
 
@@ -15,6 +17,20 @@ def submit():
     data = request.get_json()
     name = data.get("name", "Guest")
     return jsonify({"message": f"Hello, {name}!"})
+
+@app.route("/api/fares-tfl", methods=["GET"])
+def fares_tfl():
+    from_station = request.args.get("from_station")
+    to_station = request.args.get("to_station")
+    railcard_param = request.args.get("railcard", "false").lower()
+    railcard = railcard_param in ["true", "1", "yes"]
+    
+    # Use fixed time (e.g. 1630) and a flag (True) similar to your FastAPI example
+    fares = rp.getTfLDict(from_station, to_station, 1630, railcard, True)
+    return jsonify({
+        "tfl_fares": fares[0],
+        "national_rail_fares": fares[1]
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
